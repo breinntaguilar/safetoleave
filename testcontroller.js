10f037ccaa887983c2cf009a9902864a04ee4998
Breinn Aguilar <breinntaguilar@gmail.com>
2015-11-13 18:25:41 +0800

    Yolo

---
+++ testcontroller.js
@@ -0,0 +1,20 @@
+angular.module('testApp', [])
+  .controller('testController', function($scope) {
+    $scope.safeToGo = false;
+    $scope.timeDiff = 0;
+    //$scope.timeNow = null;
+    $scope.timeOut = null;
+    $scope.$watch("myTime", function(){
+      if ($scope.myTime){
+        var temp = new Date();
+        $scope.timeOut = new Date($scope.myTime.getTime()+9.5*3600*1000);
+        $scope.timeOut.setDate(temp.getDate());
+        $scope.timeOut.setFullYear(temp.getFullYear());
+        $scope.timeOut.setMonth(temp.getMonth());
+          console.log($scope.timeOut);
+          $scope.safeToGo = new Date() > $scope.timeOut;
+        
+        $scope.timeDiff = (($scope.timeOut.getTime() - Date.now()) / 1000 / 3600).toFixed(1) ;
+      }
+    });
+  });
